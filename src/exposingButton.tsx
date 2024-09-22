f745b3a9ad8a67fb53bdb33286ce65b8f141d25a
srikarreddy1998 <master.srikarreddy@gmail.com>
2024-09-22 23:57:08 +0530

    installed vite-federation, exposed a custom button

---
+++ src/exposingButton.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function Button() {
+  return (
+    <>
+      <div>hi, this is sharing button from components repo</div>
+      <button>Click me</button>
+    </>
+  );
+}
+
+export default Button;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "components-app",
+      filename: "remoteEntry.js",
+      exposes: {
+        // "./App": "./src/App.tsx", // we can also export the entire repo or route it in container as required
+        "./ExposedButton": "./src/components/ui/button.tsx", //this button contains shad cn button and tailwind is required, so it wasnt visible in container app
+        "./ExposingButton": "./src/exposingButton.tsx", // just a regular button with embedded css
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    outDir: "dist", // Ensure output directory is correct
+    target: "esnext",
+  },
+  server: {
+    port: 4173, // Ensure the port matches
+    fs: {
+      allow: ["dist"],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
